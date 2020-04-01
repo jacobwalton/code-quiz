b1a5c0c8cfd449c51794c8e8caef2c87f5e2c13f
Jacob Walton <jawalton6616@gmail.com>
2020-04-01 09:38:10 -0500

    started script, declared vars, added notes

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-var 
+// var timeLeft = document.getElementById("timer")
+// var startButton = document.getElementById("start-button")
+var questions = document.getElementById("questions")
+var choices = document.getElementById("choices")
+var notification = document.getElementById("notification")
+var completePage = document.getElementsByClassName("complete-page")
+var score = document.getElementById("score")
+var nameInput = document.querySelector("#name-input")
+var submit = document.getElementById("submit")
+var highScores = document.getElementsByClassName("high-scores")
+var scoreTable = document.getElementById("score-table")
+var startPage = document.querySelector("#start-page")
+var questionPage = document.querySelector("#questions")
+
+
+document.getElementById("start-button").addEventListener("click",function(){
+
+timeLeft=75;
+
+var triggerTime = setInterval(function(){
+    document.getElementById("timer").innerHTML = timeLeft--;
+    console.log(timeLeft);
+    if (timeLeft == 0)
+        clearInterval(triggerTime);
+
+}, 1000);
+startPage.style.display = "none";
+questionPage.style.display = "block"
+
+
+});
